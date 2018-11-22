6e54bbbe101e93a6da9508a018fecabc997ac5d5
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-11-21 23:15:38 -0500

    Fixed folder structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,8 +5,8 @@ import { HeaderComponent } from './header/header.component';
 import { HeaderBrandComponent } from './header/header-brand/header-brand.component';
 import { HeaderItemsComponent } from './header/header-items/header-items.component';
 import { HeaderItemComponent } from './header/header-items/header-item/header-item.component';
-import { HamburgerMenuComponent } from '../header/header-items/hamburger-menu/hamburger-menu.component';
-import { SideDrawerComponent } from '../header/header-items/side-drawer/side-drawer.component';
+import { HamburgerMenuComponent } from './header/header-items/hamburger-menu/hamburger-menu.component';
+import { SideDrawerComponent } from './header/header-items/side-drawer/side-drawer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
