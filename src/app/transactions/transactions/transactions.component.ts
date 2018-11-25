fe4a664852efb9de34af19f3b1c40711c0ebbec5
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-11-25 14:02:13 -0500

    Added store to transactions component

--- src/app/transactions/transactions/transactions.component.ts
+++ src/app/transactions/transactions/transactions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import * as fromTransactions from '../store/transactions.reducers';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-transactions',
@@ -6,10 +10,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
+  transactionsState: Observable<fromTransactions.State>;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.transactionsState = this.store.select('transactions');
   }
 
 }
