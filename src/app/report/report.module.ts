33bd5701d750ad57df63605071add50e6ccf277e
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-12-16 14:28:15 -0500

    Cleaned comments

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportRoutingModule } from './report-routing.module';
 import { MonthReportComponent } from './containers/month-report/month-report.component';
-// import { TransactionsModule } from '../transactions/transactions.module';
 import { AnnualReportComponent } from './containers/annual-report/annual-report.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { ChartModule } from 'angular2-chartjs';
@@ -24,7 +23,6 @@ import { CoreModule } from '../core/core.module';
   imports: [
     CommonModule,
     ReportRoutingModule,
-    // TransactionsModule,
     ChartModule,
     SharedModule,
     CoreModule
