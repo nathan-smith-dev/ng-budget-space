5eeba5e3a1e616c784309f0afb9297b219e640d3
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-12-30 14:01:43 -0500

    Removed unused imports

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -3,11 +3,6 @@ import { Store } from '@ngrx/store';
 import * as fromTransactions from '../store/transactions';
 import { Observable } from 'rxjs';
 import { Transaction } from '../shared/models/transaction.model';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { Expense } from '../shared/models/expense.model';
-import { Income } from '../shared/models/income.model';
-import * as TransactionActions from '../store/transactions/actions';
 import { Router } from '@angular/router';
 import { routerFadeAnimation } from '../shared/animations/app.animations';
 
@@ -19,15 +14,6 @@ import { routerFadeAnimation } from '../shared/animations/app.animations';
 })
 export class TransactionsComponent implements OnInit {
   transactions$: Observable<Transaction[]>;
-  transaction: Transaction = new Transaction(
-    null,
-    null,
-    null,
-    null,
-    new Date(),
-    '',
-    'Expense'
-  );
 
   constructor(private store: Store<any>, private router: Router) {}
 
